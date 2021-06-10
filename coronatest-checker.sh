eeae5a389e629578d69f0e628759068e2fd78377
Andy Repton <a.repton@gmail.com>
2021-06-10 23:47:08 +0200

    1990 eligible today

--- coronatest-checker.sh
+++ coronatest-checker.sh
@@ -4,7 +4,7 @@
 
 mkdir -p /tmp/years-checked
 
-for YEAR in $(seq 88 91); do
+for YEAR in $(seq 90 95); do
   if [ -f /tmp/years-checked/$YEAR ]; then
     echo "Already checked the year $YEAR"
   else
