bfd0aa387bc6cc4ee8b3f3841322960812f8e9ff
Andy Repton <a.repton@gmail.com>
2021-12-21 18:41:05 +0100

    Adding a rate limit check in and adding all years up to 1999

--- coronatest-checker.sh
+++ coronatest-checker.sh
@@ -4,11 +4,15 @@
 
 mkdir -p /tmp/years-checked
 
-for YEAR in $(seq 65 70); do
+for YEAR in $(seq 65 99); do
   if [ -f /tmp/years-checked/$YEAR ]; then
     echo "Already checked the year $YEAR"
   else
-    # Sometimes we get hit by a rate limit
+    # Sometimes we get hit by a rate limit. The sed is required because the rate limit message is invalid json (single instead of double quotes)
+    if curl -s "https://user-api.coronatest.nl/vaccinatie/programma/booster/1965/NEE" | sed 's/'\''/"/g' | jq -e 'select(.msg != null)'; then
+      # Rate limit exceeded. Sleep 30 seconds before continuing
+      sleep 30
+    fi
     if curl -s "https://user-api.coronatest.nl/vaccinatie/programma/booster/19${YEAR}/NEE" | jq -e 'select(.success==true)'; then
       MESSAGE="19${YEAR} is now available for vaccination"
       if [ -n "$SLACK_WEBHOOK" ]; then
@@ -19,4 +23,6 @@ for YEAR in $(seq 65 70); do
       touch /tmp/years-checked/${YEAR}
     fi
   fi
+  # Let's sleep for 3 seconds anyway just to give the poor API some love
+  sleep 3
 done
