edef080f28c0085f168b9805b74c9473bbebb1a1
Andy Repton <a.repton@gmail.com>
2021-12-21 18:26:14 +0100

    Probably should have tested this better first

--- coronatest-checker.sh
+++ coronatest-checker.sh
@@ -4,12 +4,12 @@
 
 mkdir -p /tmp/years-checked
 
-for YEAR in $(seq 65 66 67 68 69 70); do
+for YEAR in $(seq 65 70); do
   if [ -f /tmp/years-checked/$YEAR ]; then
     echo "Already checked the year $YEAR"
   else
     # Sometimes we get hit by a rate limit
-    if curl -s "https://user-api.coronatest.nl/vaccinatie/programma/programma/19${YEAR}/NEE" | jq -e 'select(.success==true)'; then
+    if curl -s "https://user-api.coronatest.nl/vaccinatie/programma/booster/19${YEAR}/NEE" | jq -e 'select(.success==true)'; then
       MESSAGE="19${YEAR} is now available for vaccination"
       if [ -n "$SLACK_WEBHOOK" ]; then
         curl -X POST -H 'Content-type: application/json' --data "{\"text\": \"${MESSAGE}\" }" ${SLACK_WEBHOOK}
